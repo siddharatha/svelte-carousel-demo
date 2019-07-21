bac65069443283e5f79541f01efdc7ae07afb14e
Sid <siddharatha.n@gmail.com>
2019-07-21 10:04:28 +0200

    move to index

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script>
-	import {contentinfo} from './content.js';
-	import Carousel from './Carousel.svelte';
-	import CarouselItem from './CarouselItem.svelte';
-	import Paginator from './Paginator.svelte';
-	
-	export let visibleindex = 0;
+  import { contentinfo } from "./content.js";
+  import Carousel from "./Carousel.svelte";
+  import CarouselItem from "./CarouselItem.svelte";
+  import Paginator from "./Paginator.svelte";
+
+  export let visibleindex = 0;
 </script>
 
 <Carousel items={contentinfo} let:blah bind:visibleindex>
-	<CarouselItem {...blah} /> 
-	<Paginator bind:visibleindex items={contentinfo}/>
-	<p>showing Index {visibleindex}</p>
+  <CarouselItem {...blah} />
 </Carousel>
+<p>showing Index {visibleindex}</p>
+<Paginator bind:visibleindex items={contentinfo} />
