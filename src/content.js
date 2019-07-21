7f91565fcc013a2cd661eb73ed00078469e0346f
Sid <siddharatha.n@gmail.com>
2019-07-21 11:11:42 +0200

    update content

---
+++ src/content.js
@@ -0,0 +1,26 @@
+export const contentinfo = [
+  {
+    description: "Random text with slide 1",
+    imageUrl: "https://picsum.photos/200",
+    id: 1,
+    buttons: [{ label: "Continue", type: "continue" }]
+  },
+  {
+    description: "Random text with slide 2",
+    imageUrl: "https://picsum.photos/200/300",
+    id: 2,
+    buttons: [{ label: "Continue", type: "continue" }]
+  },
+  {
+    description: "Random text with slide 3",
+    imageUrl: "https://picsum.photos/200/400",
+    id: 3,
+    buttons: [{ label: "Continue", type: "continue" }]
+  },
+  {
+    description: "Random text with slide 4",
+    imageUrl: "https://picsum.photos/200/500",
+    id: 4,
+    buttons: [{ label: "Back", type: "other" }]
+  }
+];
